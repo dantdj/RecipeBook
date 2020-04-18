5ace7e600f968ad50f19267f5db3a1c082c35e9b
Daniel Temple <dantdj@gmail.com>
2020-04-18 19:20:02 +0100

    Add placeholder "Show Recipe" endpoint

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func (app *application) ping(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(writer, "OK!")
 }
+
+func (app *application) showRecipe(writer http.ResponseWriter, request *http.Request) {
+	id, err := strconv.Atoi(request.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.serverError(writer, err)
+		return
+	}
+	fmt.Fprintf(writer, "Will display recipe with ID %d in future...", id)
+}
+
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,15 @@
 package main 
+package main
 
 import "net/http"
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	// Status
 	mux.HandleFunc("/ping", app.ping)
 
+	// Recipe operations
+	mux.HandleFunc("/recipe", app.showRecipe)
 	return mux
 }
